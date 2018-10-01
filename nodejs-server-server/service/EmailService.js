24b1b989450ad6eae52302b1e40d7b1bed5845c7
Olof93 <erik.olofsson93@outlook.com>
2018-10-01 21:22:17 +0200

    swagger Erik

---
+++ nodejs-server-server/service/EmailService.js
@@ -0,0 +1,28 @@
+const db = require("mongoose");
+const encrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const EmailSent = require("../../../models/graphs/emailSent/emailSent");
+
+exports.getType = (req, res) => {
+    EmailSent.find()
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(error => res.status(500).json({ errorcode: "500", message: "internal server error"}))
+}
+
+exports.placeOrder = (req, res) => {
+    let emailSent = new EmailSent({
+        _id: new db.Types.ObjectId()
+    });
+    emailSent.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({ message: "Product was created successfully" })
+        })
+        .catch(error => console.log(error));
+}
+    
